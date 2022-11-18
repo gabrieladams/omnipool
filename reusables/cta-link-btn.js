4c8310a2415f7a8c4b9d74093fd80c6ac0fbf38f
gabrieladams <gabrieladams1738@gmail.com>
2022-11-18 13:48:21 +0300

    routed invest-now link to /sign-up

--- reusables/cta-link-btn.js
+++ reusables/cta-link-btn.js
@@ -3,14 +3,14 @@ import Link from "next/link";
 function CtaBtnLink({ className }) {
   if (className) {
     return (
-      <Link href="#" className={`site-btn site-btn-md ${className}`}>
+      <Link href="/sign-up" className={`site-btn site-btn-md ${className}`}>
         Invest Now
       </Link>
     );
   }
 
   return (
-    <Link href="#" className="site-btn site-btn-md">
+    <Link href="/sign-up" className="site-btn site-btn-md">
       Invest Now
     </Link>
   );
